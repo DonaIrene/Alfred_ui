0debd9cd5139b5286ed57d938d91958179e00b7f
Pão de Queijo <78106075+DonaIrene@users.noreply.github.com>
2021-12-13 15:52:41 +0000

    Update Autolocate_downloads.exe.py

--- Autolocate_downloads.exe.py
+++ Autolocate_downloads.exe.py
@@ -2,15 +2,15 @@ import os
 import shutil
 import glob
 
-source = 'C:\\Users\\Lucas Costa\\Downloads\\'
+source = 'C:\\Users\\Pao_de_Queijo\\Downloads\\'
 my_dirct = {
-    'C:\\Users\\Lucas Costa\\Downloads\\Imagens': ['jpg','png','gif'],
-    'C:\\Users\\Lucas Costa\\Downloads\\Zippadas': ['rar','zip','7z','tar'],
-    'C:\\Users\\Lucas Costa\\Downloads\\Executaveis': ['exe','msi'],
-    'C:\\Users\\Lucas Costa\\Downloads\\PDF':['pdf','docx', 'doc', 'xls','txt'],
-    'C:\\Users\\Lucas Costa\\Downloads\\Musicas':['mp3','mp4'],
-    'C:\\Users\\Lucas Costa\\Downloads\\Outros' : ['rmskin','dll','ini','jar','properties','bat'],
-    'C:\\Users\\Lucas Costa\\Downloads\\Torrent' : ['torrent']
+    'C:\\Users\\Pao_de_Queijo\\Downloads\\Imagens': ['jpg','png','gif'],
+    'C:\\Users\\Pao_de_Queijo\\Downloads\\Zippadas': ['rar','zip','7z','tar'],
+    'C:\\Users\\Pao_de_Queijo\\Downloads\\Executaveis': ['exe','msi'],
+    'C:\\Users\\Pao_de_Queijo\\Downloads\\PDF':['pdf','docx', 'doc', 'xls','txt'],
+    'C:\\Users\\Pao_de_Queijo\\Downloads\\Musicas':['mp3','mp4'],
+    'C:\\Users\\Pao_de_Queijo\\Downloads\\Outros' : ['rmskin','dll','ini','jar','properties','bat'],
+    'C:\\Users\\Pao_de_Queijo\\Downloads\\Torrent' : ['torrent']
 }
 for destination, extensions in my_dirct.items():
     for ext in extensions:
