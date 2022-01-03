05451d6d39b24bbe6f497292c76bc10de7a42ad7
Pão de Queijo <78106075+DonaIrene@users.noreply.github.com>
2022-01-03 17:23:57 +0000

    sad

--- Autolocate_downloads.exe.py
+++ Autolocate_downloads.exe.py
@@ -4,13 +4,16 @@ import glob
 
 source = 'C:\\Users\\Pao_de_Queijo\\Downloads\\'
 my_dirct = {
+    'D:\\Tokio_School\\Python\\' : ['ipynb'],
     'C:\\Users\\Pao_de_Queijo\\Downloads\\Imagens': ['jpg','png','gif'],
     'C:\\Users\\Pao_de_Queijo\\Downloads\\Zippadas': ['rar','zip','7z','tar'],
     'C:\\Users\\Pao_de_Queijo\\Downloads\\Executaveis': ['exe','msi'],
     'C:\\Users\\Pao_de_Queijo\\Downloads\\PDF':['pdf','docx', 'doc', 'xls','txt'],
     'C:\\Users\\Pao_de_Queijo\\Downloads\\Musicas':['mp3','mp4'],
+    'C:\\Users\\Pao_de_Queijo\\Downloads\\Torrent' : ['torrent'],
+
+    #Aconselhavel deixar (outros) no final do set, pois pode criar conflito ao mover ficheiros#
     'C:\\Users\\Pao_de_Queijo\\Downloads\\Outros' : ['rmskin','dll','ini','jar','properties','bat'],
-    'C:\\Users\\Pao_de_Queijo\\Downloads\\Torrent' : ['torrent']
 }
 for destination, extensions in my_dirct.items():
     for ext in extensions:
